da1f07e7e3eafa258bbf761e14bad14413810a8b
Neelakandan18 <neelakandan.c@unshots.com>
2023-05-15 12:53:10 +0530

    jquery script tag changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" />
+        <link
+          href="https://propstory.in/universe-chat-bot-html-template/css/worldofjoy-chatbot.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
