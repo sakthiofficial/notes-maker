35e409772170bac4c93799e9e4a057eb6ec766a2
Neelakandan18 <neelakandan.c@unshots.com>
2023-05-18 17:23:27 +0530

    jquery changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,12 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" />
-        <link
-          href="https://propstory.in/universe-chat-bot-html-template/css/worldofjoy-chatbot.css"
-          rel="stylesheet"
-          type="text/css"
-        />
+       
       </Head>
       <body>
         <Main />
