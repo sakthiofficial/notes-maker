143d28f7b4ec7f43cf70bd49b0aaad6e69ca224c
bhokray <ketan.b@alliancein.com>
2023-06-02 01:20:57 +0530

    browser url; brochure

---
+++ src/pages/brochure.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Brochure() {
+  return (
+    <iframe
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+      title="brochure"
+      src="/The-World-of-Joy-Brochure.pdf"
+    />
+  );
+}
+
+export default Brochure;
